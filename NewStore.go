2eb1d63deccd1615ecfa5b20cc2cc96788ad0555
Milan Lesichkov <lesichkovm@gmail.com>
2024-02-19 05:10:29 +0000

    Updates

---
+++ NewStore.go
@@ -0,0 +1,49 @@
+package blogstore
+
+import (
+	"database/sql"
+	"errors"
+
+	"github.com/gouniverse/sb"
+)
+
+// NewStoreOptions define the options for creating a new block store
+type NewStoreOptions struct {
+	PostTableName      string
+	DB                 *sql.DB
+	DbDriverName       string
+	TimeoutSeconds     int64
+	AutomigrateEnabled bool
+	DebugEnabled       bool
+}
+
+// NewStore creates a new block store
+func NewStore(opts NewStoreOptions) (*Store, error) {
+	if opts.PostTableName == "" {
+		return nil, errors.New("blog store: PostTableName is required")
+	}
+
+	if opts.DB == nil {
+		return nil, errors.New("blog store: DB is required")
+	}
+
+	if opts.DbDriverName == "" {
+		opts.DbDriverName = sb.DatabaseDriverName(opts.DB)
+	}
+
+	store := &Store{
+		postTableName:      opts.PostTableName,
+		automigrateEnabled: opts.AutomigrateEnabled,
+		db:                 opts.DB,
+		dbDriverName:       opts.DbDriverName,
+		debugEnabled:       opts.DebugEnabled,
+	}
+
+	store.timeoutSeconds = 2 * 60 * 60 // 2 hours
+
+	if store.automigrateEnabled {
+		store.AutoMigrate()
+	}
+
+	return store, nil
+}
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# blogstore
+# Blog Store
+
+<a href="https://gitpod.io/#https://github.com/gouniverse/blogstore" style="float:right:"><img src="https://gitpod.io/button/open-in-gitpod.svg" alt="Open in Gitpod" loading="lazy"></a>
+
+[![Tests Status](https://github.com/gouniverse/blogstore/actions/workflows/test.yml/badge.svg?branch=main)](https://github.com/gouniverse/blogstore/actions/workflows/test.yml)
+[![Go Report Card](https://goreportcard.com/badge/github.com/gouniverse/blogstore)](https://goreportcard.com/report/github.com/gouniverse/blogstore)
+[![PkgGoDev](https://pkg.go.dev/badge/github.com/gouniverse/blogstore)](https://pkg.go.dev/github.com/gouniverse/blogstore)
+
+Stores blog posts to a database table.
+
+## Installation
+```
+go get -u github.com/gouniverse/blogstore
+```
+
+## Setup
+
+```go
+blogStore = blogstore.NewStore(blog.NewStoreOptions{
+	DB:                 databaseInstance,
+	PostTableName:     "blog_post",
+	AutomigrateEnabled: true,
+	DebugEnabled:       false,
+})
+```
